38825a53ff04398eaab663f8e7b65938d438a11a
gabriel337 <g.david.lopezruiz@gmail.com>
2022-05-22 01:34:41 +0000

    add taks 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,3 +13,19 @@ def status():
         return JSON of OK status
     '''
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('stats')
+def new_count():
+    """endpoint that retrieves the number of each objects by type"""
+    dic = {
+
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)
+            }
+    return jsonify(dic)
+
