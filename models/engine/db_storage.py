885ad7ea35a37dbc841c8c49c881757b85939066
gabriel337 <g.david.lopezruiz@gmail.com>
2022-05-22 01:18:16 +0000

    added new line

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -86,3 +86,4 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.close()
+
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -77,3 +77,4 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
