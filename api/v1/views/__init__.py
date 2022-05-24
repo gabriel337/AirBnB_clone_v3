41f6b3e1d0839c39dcbad9e9bd9e1b2525bf0734
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-05-24 03:23:30 +0000

    added view for place, uncommented import statement

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,7 +10,7 @@ import api.v1.views.states
 import api.v1.views.cities
 import api.v1.views.amenities
 import api.v1.views.users
-#import api.v1.views.places
+import api.v1.views.places
 #import api.v1.views.places_reviews
 #import api.v1.views.places_amenities
 #from api.v1.views.states import *
